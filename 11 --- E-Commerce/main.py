fa028b325f35f44eb3901cc27db72026b11ef1f7
Hritik-Bajpeyee <hritikbajpeyee@gmail.com>
2025-03-12 20:31:58 +0530

    make File

---
+++ 11 --- E-Commerce/main.py
@@ -0,0 +1,5 @@
+'''
+Develop a full-stack e-commerce platform.
+Include features like product search, user authentication, and a shopping cart.
+Use Django/Flask for the backend and HTML/CSS for the frontend
+'''
